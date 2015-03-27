f1ff9f76abc3297c553da2585be28b3fd22f60bd
kouimtzis <pm@kouimtzis.gr>
2015-03-27 09:13:29 +0200

    0.001.150327.01 new menu layout

--- ExtraTools/Form1.Designer.vb
+++ ExtraTools/Form1.Designer.vb
@@ -24,11 +24,11 @@ Partial Class Form1
     Private Sub InitializeComponent()
         Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
         Me.StartToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ItemsToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.CustomerToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.SupplierToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.PurchasesToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.SalesToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
-        Me.ItemsToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.MantisToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.EshopToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.MenuStrip1.SuspendLayout()
@@ -39,7 +39,7 @@ Partial Class Form1
         Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.StartToolStripMenuItem, Me.ItemsToolStripMenuItem, Me.CustomerToolStripMenuItem, Me.SupplierToolStripMenuItem, Me.PurchasesToolStripMenuItem, Me.SalesToolStripMenuItem, Me.MantisToolStripMenuItem, Me.EshopToolStripMenuItem})
         Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
         Me.MenuStrip1.Name = "MenuStrip1"
-        Me.MenuStrip1.Size = New System.Drawing.Size(1240, 24)
+        Me.MenuStrip1.Size = New System.Drawing.Size(1262, 24)
         Me.MenuStrip1.TabIndex = 1
         Me.MenuStrip1.Text = "MenuStrip1"
         '
@@ -48,6 +48,12 @@ Partial Class Form1
         Me.StartToolStripMenuItem.Name = "StartToolStripMenuItem"
         Me.StartToolStripMenuItem.Size = New System.Drawing.Size(56, 20)
         Me.StartToolStripMenuItem.Text = "Αρχική"
+        '
+        'ItemsToolStripMenuItem
+        '
+        Me.ItemsToolStripMenuItem.Name = "ItemsToolStripMenuItem"
+        Me.ItemsToolStripMenuItem.Size = New System.Drawing.Size(69, 20)
+        Me.ItemsToolStripMenuItem.Text = "Αποθήκη"
         '
         'CustomerToolStripMenuItem
         '
@@ -73,12 +79,6 @@ Partial Class Form1
         Me.SalesToolStripMenuItem.Size = New System.Drawing.Size(73, 20)
         Me.SalesToolStripMenuItem.Text = "Πωλήσεις"
         '
-        'ItemsToolStripMenuItem
-        '
-        Me.ItemsToolStripMenuItem.Name = "ItemsToolStripMenuItem"
-        Me.ItemsToolStripMenuItem.Size = New System.Drawing.Size(69, 20)
-        Me.ItemsToolStripMenuItem.Text = "Αποθήκη"
-        '
         'MantisToolStripMenuItem
         '
         Me.MantisToolStripMenuItem.Name = "MantisToolStripMenuItem"
@@ -95,7 +95,7 @@ Partial Class Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(1240, 368)
+        Me.ClientSize = New System.Drawing.Size(1262, 507)
         Me.Controls.Add(Me.MenuStrip1)
         Me.MainMenuStrip = Me.MenuStrip1
         Me.Name = "Form1"
