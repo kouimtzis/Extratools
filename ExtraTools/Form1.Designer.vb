18a4fbdc8d16ae0164b286658eefe376653256a1
kouimtzis <pm@kouimtzis.gr>
2015-03-27 11:51:16 +0200

    0.001.150327.03 bla bla

--- ExtraTools/Form1.Designer.vb
+++ ExtraTools/Form1.Designer.vb
@@ -95,7 +95,7 @@ Partial Class Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(1262, 507)
+        Me.ClientSize = New System.Drawing.Size(1262, 529)
         Me.Controls.Add(Me.MenuStrip1)
         Me.MainMenuStrip = Me.MenuStrip1
         Me.Name = "Form1"
